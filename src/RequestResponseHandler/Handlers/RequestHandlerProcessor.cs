82b299ea7112121dcf79deadefd2ed9df70c4061
Ed Chaparro <ed@IntrepidProducts.com>
2022-08-27 22:30:28 -0400

    Add reference to IocContainer
    Will be used to resolve request handlers and other dependencies.

---
+++ src/RequestResponseHandler/Handlers/RequestHandlerProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using IntrepidProducts.IocContainer;
+
+namespace IntrepidProducts.RequestResponseHandler.Handlers
+{
+    public class RequestHandlerProcessor : RequestHandlerProcessorAbstract
+    {
+        public RequestHandlerProcessor
+            (IRequestHandlerRegistry requestHandlerRegistry, IIocContainer iocContainer)
+            : base(requestHandlerRegistry)
+        {
+            _iocContainer = iocContainer;
+        }
+
+        private readonly IIocContainer _iocContainer;
+
+        protected override IRequestHandler? Resolve(Type requestHandlerType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
