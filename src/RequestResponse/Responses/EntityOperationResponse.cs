b9444f177b6eb5f77b44bd5246b1a69130dc1e61
Ed Chaparro <ed@IntrepidProducts.com>
2022-10-02 10:29:32 -0400

    Added Entity Operation Response class
    May be useful for Entity CRUD operations

---
+++ src/RequestResponse/Responses/EntityOperationResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using IntrepidProducts.RequestResponse.Requests;
+
+namespace IntrepidProducts.RequestResponse.Responses
+{
+    public class EntityOperationResponse : OperationResponse
+    {
+        public EntityOperationResponse(IRequest originalRequest, ErrorInfo? errorInfo = null) : base(originalRequest, errorInfo)
+        { }
+
+        public Guid EntityId { get; set; }
+    }
+}
